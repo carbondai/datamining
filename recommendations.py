2b8aee9580a551ffed7bcb80bea4fabbe252a260
daixin <daixin2015@icloud.com>
2016-07-28 15:26:37 +0800

    增加皮尔逊评价度方法，修正person_name列表内容

--- recommendations.py
+++ recommendations.py
@@ -22,6 +22,7 @@ critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snake on a Plane':3.5,
     'Toby': {'Snakes on a Plane':4.5, 'Superman Returns': 4.0, 
     'You, Me and Dupree': 1.0}}
 
+
 def sim_distance(prefs, person1, person2):
     """
     欧几里德距离
@@ -36,14 +37,60 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
         for item in prefs[person1] if item in prefs[person2]])
 
-    return 1 / (1 + sqrt(sum_of_squares))
+    return round(1 / (1 + sqrt(sum_of_squares)), 3)
 
 
-#person_name = critics.keys()
-pairs = [(i, j) for i in critics for j in critics if i != j]
+def sim_pearson(prefs, p1, p2):
+    """
+    皮尔逊相关度评价
+    """
+    # 得到双方都曾评价过的物品列表
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    n = len(si)
+    if n == 0:
+        return 1
+
+    # 对所有偏好求和
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    # 求平方和
+    sq_sum1 = sum([pow(prefs[p1][it], 2) for it in si])
+    sq_sum2 = sum([pow(prefs[p2][it], 2) for it in si])
+
+    # 求乘积之和
+    p_sum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+
+    # 计算皮尔逊评价值
+    num = p_sum - (sum1 * sum2 / n)
+    den = sqrt((sq_sum1 - pow(sum1, 2)/n)*(sq_sum2 - pow(sum2, 2)/n))
+    if den == 0:
+        return 0
+    r = num / den
+
+    return round(r, 3)
+
+
+person_name = critics.keys()
+length = len(person_name)
+
+# 将一个列表中元素两两组成元组， 想想有没有更简洁的表达
+pairs = [(person_name[i], person_name[j]) for i in range(length) for j in range(length) if i < j]
+
 person_dis = {}
 for item in pairs:
     person_dis[item] = sim_distance(critics, *item)
 
 sorted_person_dis = sorted(person_dis.items(), key=lambda x:x[1], reverse=True)
 print sorted_person_dis
+
+
+pearson_dis = {}
+for item in pairs:
+    pearson_dis[item] = sim_pearson(critics, *item)
+sorted_pearson_dis = sorted(pearson_dis.items(), key=lambda x:x[1], reverse=True)
+print sorted_pearson_dis
